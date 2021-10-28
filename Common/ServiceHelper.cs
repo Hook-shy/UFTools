dc8f02f74d606919862b05dd105d5f5a1d94dba2
shy <2206034346@qq.com>
2021-10-28 08:31:21 +0800

    完善和服务相关的功能

---
+++ Common/ServiceHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UFTools.Common
+{
+    public static class ServiceHelper
+    {
+        public static bool RestartService(ServiceController service)
+        {
+            try
+            {
+                if (service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped);
+                }
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                LogHelper.WriteLog($"{ex.Message}\n重启服务{service.ServiceName}");
+                return false;
+            }
+        }
+
+        public static List<ServiceController> GetLocalSqlServices() => ServiceController.GetServices().Where(s => s.Status == ServiceControllerStatus.Running && new Regex(@"SQL Server \(.*\)", RegexOptions.IgnoreCase).IsMatch(s.DisplayName)).ToList();
+    }
+}
