8b804dc236f3f24dcd31515410e53621b44a70ae
shy <2206034346@qq.com>
2021-10-28 08:36:39 +0800

    优化日志模块

--- Common/LogHelper.cs
+++ Common/LogHelper.cs
@@ -9,14 +9,14 @@ namespace UFTools.Common
 {
     public class LogHelper
     {
-        public static void WriteLog(string connect)
+        public static void WriteLog(string logTxt)
         {
             try
             {
                 using (StreamWriter sw = new StreamWriter($@"{Environment.CurrentDirectory}\Log.txt", true, Encoding.UTF8))
                 {
                     string sign = "===================================================";
-                    sw.WriteLine($"\n{sign}{DateTime.Now}{sign}\n{connect}");
+                    sw.WriteLine($"\n{sign}{DateTime.Now}{sign}\n{logTxt}");
                 }
             }
             catch (Exception ex)
